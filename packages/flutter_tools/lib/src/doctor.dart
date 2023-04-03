da9edf32d3cd2e0a9256ec6727e32b27f7cbc5bd
Adam <a.kaczmarzyk@codigee.com>
2023-04-03 22:05:46 +0200

    flutter doctor bugfix

--- packages/flutter_tools/lib/src/doctor.dart
+++ packages/flutter_tools/lib/src/doctor.dart
@@ -99,10 +99,6 @@ class _DefaultDoctorValidatorsProvider implements DoctorValidatorsProvider {
     featureFlags: featureFlags,
   );
 
-  late final CustomDeviceWorkflow customDeviceWorkflow = CustomDeviceWorkflow(
-    featureFlags: featureFlags,
-  );
-
   @override
   List<DoctorValidator> get validators {
     if (_validators != null) {
